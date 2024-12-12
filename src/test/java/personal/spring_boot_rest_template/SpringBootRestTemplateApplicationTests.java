8badbac1afcbd88f8061f8fc02de9ccc3afbbf52
trongnhatt <cuongnt.dev.2501@gmail.com>
2024-12-12 11:46:08 +0700

    init

---
+++ src/test/java/personal/spring_boot_rest_template/SpringBootRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.spring_boot_rest_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
